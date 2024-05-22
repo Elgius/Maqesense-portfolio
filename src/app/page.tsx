96e4dc018c4d70dfc591b526cc02e6b19b035ed2
Elgius <114131653+Elgius@users.noreply.github.com>
2024-05-22 15:34:22 +0500

    Hero section finalised

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { AuroraBackground } from "@/components/animations/aurora-background";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-black to-gray-600 overflow-hidden">
-      <div>
-        <div>
-          <h1>Welcome to Maqesense</h1>
+    // <main className="overflow-hidden bg-gray-700 min-h-screen">
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4 "
+      >
+        <div className="text-3xl md:text-7xl font-bold text-white text-center">
+          Welcome to Maqesense!
         </div>
-      </div>
-    </main>
+        <div className="font-extralight text-base md:text-4xl text-neutral-200 py-4">
+          This is our internal tools site! gtfo if you not supposed to be here
+        </div>
+        <Button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+          Portfolio
+        </Button>
+      </motion.div>
+    </AuroraBackground>
+    // </main>
   );
 }
